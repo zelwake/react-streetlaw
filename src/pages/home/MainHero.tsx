030fe1cc5f658be55f5cdad46c1333a6f91c0384
Michal Hladík <michalhladik2@seznam.cz>
2023-05-15 18:44:48 +0200

    hero for mobile

--- src/pages/home/MainHero.tsx
+++ src/pages/home/MainHero.tsx
@@ -8,11 +8,23 @@ const MainHero = () => {
   if (!hero) return <p>Načítání fotografie...</p>
 
   return (
-    <div>
-      <p>{hero.title}</p>
-      <img src={hero.url} alt={hero.title} />
-      <p>{hero.title}</p>
-    </div>
+    <>
+      {/* Mobile Hero */}
+      <div>
+        <image className="block bg-streetlaw-500 w-screen pt-1">
+          <img src={hero.url} alt={hero.title} className="m-auto max-w-xs md:max-w-2xl" />
+        </image>
+        <p className="w-full bg-streetlaw-500 text-center text-white text-2xl font-bold py-6 px-10">
+          {hero.title}
+        </p>
+        {/* PC Hero */}
+      </div>
+      <div className="max-lg:hidden">
+        <p>{hero.title}</p>
+        <img src={hero.url} alt={hero.title} />
+        <p>{hero.title}</p>
+      </div>
+    </>
   )
 }
 
