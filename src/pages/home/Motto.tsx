065a245f706f0c04653f7c0ba8326660b52cbfeb
Michal Hladík <michalhladik2@seznam.cz>
2023-05-15 18:54:00 +0200

    motto

---
+++ src/pages/home/Motto.tsx
@@ -0,0 +1,9 @@
+const Motto = () => {
+  return (
+    <p className="text-4xl text-center px-8 py-8">
+      „Zažít právo jinak. Hravě a interaktivně. S&nbsp;právníky.“
+    </p>
+  )
+}
+
+export default Motto
