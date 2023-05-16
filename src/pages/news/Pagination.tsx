4fd45adca8e4913796834f3a0a28f730190b9d78
Michal Hladík <michalhladik2@seznam.cz>
2023-05-16 22:41:36 +0200

    pagination functionality and css

--- src/pages/news/Pagination.tsx
+++ src/pages/news/Pagination.tsx
@@ -9,13 +9,80 @@ const Pagination = ({
   setPageNumber: Dispatch<SetStateAction<number>>
   numberOfPages: number
 }) => {
+  const pages: JSX.Element[] = []
+  for (let i = 1; i <= numberOfPages; i++) {
+    pages.push(
+      <PaginationNavButton
+        key={i}
+        page={i}
+        setPageNumber={setPageNumber}
+        pageNumber={pageNumber}
+        numberOfPages={numberOfPages}
+      />
+    )
+  }
+
   return (
     <nav>
-      <button>&lt;&lt;</button>
-      <button>{pageNumber}</button>
-      <button>&gt;&gt;</button>
+      <div className="shadow-sl w-fit m-auto">{pages}</div>
+      <div className="shadow-sl w-fit m-auto mt-2">
+        <PaginationNavButton
+          page="down"
+          setPageNumber={setPageNumber}
+          pageNumber={pageNumber}
+          numberOfPages={numberOfPages}
+          disabled={pageNumber == 1 && true}
+        />
+        <PaginationNavButton
+          page="up"
+          setPageNumber={setPageNumber}
+          pageNumber={pageNumber}
+          numberOfPages={numberOfPages}
+          disabled={pageNumber == numberOfPages && true}
+        />
+      </div>
     </nav>
   )
 }
 
 export default Pagination
+
+const PaginationNavButton = ({
+  page,
+  setPageNumber,
+  pageNumber,
+  numberOfPages,
+  disabled,
+}: {
+  page: number | 'up' | 'down'
+  setPageNumber: Dispatch<SetStateAction<number>>
+  pageNumber: number
+  numberOfPages: number
+  disabled?: boolean
+}) => {
+  return (
+    <button
+      className={`w-10 h-10 font-semibold text-lg ${
+        pageNumber == page && 'bg-streetlaw-500 text-white'
+      } ${disabled && 'text-gray-400'}`}
+      disabled={disabled}
+      onClick={() => {
+        setPageNumber((prev) =>
+          page == 'up'
+            ? prev < numberOfPages
+              ? prev + 1
+              : prev
+            : page == 'down'
+            ? prev > 1
+              ? prev - 1
+              : prev
+            : page
+        )
+        window.scrollTo(0, 0)
+      }}>
+      {page == 'up' && <>&gt;&gt;</>}
+      {page == 'down' && <>&lt;&lt;</>}
+      {typeof page == 'number' && page}
+    </button>
+  )
+}
