20f71a062738af0a024a3debcf14217856c629aa
Michal Hladík <michalhladik2@seznam.cz>
2023-05-29 19:23:34 +0200

    pageheading and article css update for pc

--- src/components/ui/Elements/Article.tsx
+++ src/components/ui/Elements/Article.tsx
@@ -1,7 +1,7 @@
 const Article = ({ data }: { data: string }) => {
   return (
     <article
-      className="px-6 py-4 md:max-w-3xl m-auto"
+      className="px-6 py-4 lg:py-10 md:max-w-3xl xl:max-w-4xl m-auto"
       dangerouslySetInnerHTML={{ __html: data }}></article>
   )
 }
--- src/components/ui/Layout/PageHeading.tsx
+++ src/components/ui/Layout/PageHeading.tsx
@@ -7,10 +7,10 @@ const PageHeading = ({
 }) => {
   document.title = title
   return (
-    <header className="bg-streetlaw-500 text-white px-6 py-3 sm:py-6">
-      <h1 className="text-3xl font-semibold text-center">{title}</h1>
+    <header className="bg-streetlaw-500 text-white px-6 py-3 sm:py-6 lg:py-12 ">
+      <h1 className="text-3xl lg:text-4xl font-semibold text-center">{title}</h1>
       {date && (
-        <time className="text-right block mt-2">
+        <time className="text-right block relative top-8 mt-2">
           {date.day} {date.month} {date.year}
         </time>
       )}
