c16a3f285c9679a4f6cade045a3a85d0924436a1
Michal Hladík <michalhladik2@seznam.cz>
2023-05-29 18:42:05 +0200

    updated css for statistics for pc

--- src/pages/home/Statistics.tsx
+++ src/pages/home/Statistics.tsx
@@ -2,18 +2,20 @@ import { numberStatistics } from '@/data/landingStats'
 
 const Panel = ({ value, text }: { value: number; text: string }) => {
   return (
-    <li className="flex flex-col items-center gap-2 mb-6 last:mb-0">
-      <p className="w-full sm:w-40 shadow-sl bg-streetlaw-500 text-white text-center text-4xl font-bold py-3">
+    <li className="flex flex-col items-center gap-2 lg:gap-5 mb-6 sm:mb-0 last:mb-0">
+      <p className="w-full sm:w-40 lg:w-56 shadow-sl bg-streetlaw-500 text-white text-center text-4xl font-bold py-3">
         {value}
       </p>
-      <p className="w-full sm:w-40 shadow-sl text-xl py-4 px-4 text-center">{text}</p>
+      <p className="w-full sm:w-40 lg:w-56 shadow-sl text-xl lg:text-2xl py-4 lg:py-8 px-4 lg:px-6 text-center">
+        {text}
+      </p>
     </li>
   )
 }
 
 const Statistics = () => {
   return (
-    <ul className="sm:flex sm:gap-10">
+    <ul className="sm:flex sm:gap-10 lg:gap-14">
       {numberStatistics.map((e, i) => (
         <Panel key={i} {...e} />
       ))}
