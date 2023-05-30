3ffed59fdd2701984944978c4a7286f0036a76a9
Michal Hladík <michalhladik2@seznam.cz>
2023-05-30 15:10:51 +0200

    media,news heading updated to use pagehead

--- src/pages/newsPage/NewsPage.tsx
+++ src/pages/newsPage/NewsPage.tsx
@@ -1,5 +1,6 @@
 import NewsBlock from '@/components/ui/Elements/NewsBlock'
 import { NewsProps } from '@/components/ui/Elements/interface'
+import PageHeading from '@/components/ui/Layout/PageHeading'
 import { fetchPaginatedData } from '@/utils/fetchData'
 import { useQuery } from '@tanstack/react-query'
 import { useEffect, useState } from 'react'
@@ -26,21 +27,11 @@ const NewsPage = ({ category }: NewsPageProps) => {
       ),
   })
 
-  useEffect(() => {
-    if (data) {
-      setNumberOfPages(data.totalPages | 0)
-      document.title =
-        category == 2 ? 'Novinky' : category == 3 ? 'Mediální ohlasy' : 'Streetlaw.eu'
-    }
-  }, [data?.totalPages])
+  useEffect(() => data && setNumberOfPages(data.totalPages | 0), [data?.totalPages])
 
   return (
     <>
-      <header className="bg-streetlaw-500 text-white flex items-center justify-center h-20 sm:h-32">
-        <h1 className="text-4xl sm:text-5xl font-semibold">
-          {category == Category.news ? 'Aktuality' : 'Mediální ohlasy'}
-        </h1>
-      </header>
+      <PageHeading title={category == Category.news ? 'Aktuality' : 'Mediální ohlasy'} />
       <section className="px-8 my-8 sm:my-14 flex flex-col gap-10 sm:gap-14">
         <NewsBlock isLoading={isLoading} isError={isError} data={data?.res} />
         <Pagination
